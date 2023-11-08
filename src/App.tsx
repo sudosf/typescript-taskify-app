ee250e384704dfeea691c9ca59f3e37cbcdce0ee
sudosf <sudosprogrammer@gmail.com>
2023-11-08 04:24:04 +0200

    added search field

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./App.css";
+import InputField from "./components/InputField";
 
-function App() {
-    return <div>Hello World</div>;
-}
+const App: React.FC = () => {
+    
+    const [todo, setTodo] = React.useState<string>("");
+
+    return (
+        <>
+            <div className="App">
+                <span className="heading">Taskify</span>
+                <InputField todo={todo} setTodo={setTodo}/>
+            </div>
+        </>
+    );
+};
 
 export default App;
---
+++ src/components/InputField.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo }) => {
+    return (
+        <form className="input">
+            <input
+                value={todo}
+                onChange={(e) => setTodo(e.target.value)}
+                type="input"
+                placeholder="Enter a task"
+                className="input__box"
+            />
+            <button className="input_submit">Go</button>
+        </form>
+    );
+};
+
+export default InputField;
