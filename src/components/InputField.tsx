36b48d9420375d8c3d11c628c030e3ccf4e1a642
sudosf <sudosprogrammer@gmail.com>
2023-11-08 05:49:02 +0200

    tutorial complete

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
-interface Props {
+interface props {
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
     handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
     return (
-        <form className="input" onSubmit={handleAdd}>
+        <form
+            className="input"
+            onSubmit={(e) => {
+                handleAdd(e);
+                inputRef.current?.blur();
+            }}
+        >
             <input
+                type="text"
+                placeholder="Enter a Task"
                 value={todo}
+                ref={inputRef}
                 onChange={(e) => setTodo(e.target.value)}
-                type="input"
-                placeholder="Enter a task"
                 className="input__box"
             />
-            <button className="input_submit">Go</button>
+            <button type="submit" className="input_submit">
+                GO
+            </button>
         </form>
     );
 };
