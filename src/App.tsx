3a6c3b25e1f5098eeff9e848c13943cb0888a31f
sudosf <sudosprogrammer@gmail.com>
2023-11-08 04:37:24 +0200

    handle submit function

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from "react";
 import "./App.css";
 import InputField from "./components/InputField";
+import { Todo } from './models/model';
 
 const App: React.FC = () => {
     
     const [todo, setTodo] = React.useState<string>("");
+    const [todoList, setTodoList] = React.useState<Todo[]>([]);
+
+    function handleAdd(e: React.FormEvent) {
+        e.preventDefault();
+
+        if (todo) {
+            setTodoList([ ...todoList, { id: Date.now(), todo: todo, isDone: false} ])
+            setTodo("");
+        }
+    }
 
     return (
         <>
             <div className="App">
                 <span className="heading">Taskify</span>
-                <InputField todo={todo} setTodo={setTodo}/>
+                <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
             </div>
         </>
     );
